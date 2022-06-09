d3361b08ff5dbf0e31a1b0dbc4fbb6ee2ee64e6c
nishantswo <79681892+nishantswo@users.noreply.github.com>
2022-06-09 22:57:45 +0530

    Add files via upload

--- script.ps1
+++ script.ps1
@@ -9,13 +9,6 @@ $bindingInformation = "*:" + $port + ":" + $hostName
 $certificatePath = ("cert:\localmachine\my\" + $certificate.Thumbprint)
 $securedString = ConvertTo-SecureString -String $password -Force -AsPlainText
 mkdir C:\cats
-cp cats.jpg C:\cats
-cp index.html C:\cats
 Export-PfxCertificate -FilePath "C:\inetpub\temp\temp.pfx" -Cert $certificatePath -Password $securedString
 Import-PfxCertificate -FilePath "C:\inetpub\temp\temp.pfx" -CertStoreLocation "Cert:\LocalMachine\Root" -Password $securedString
 New-IISSite -Name "CatsSite" -PhysicalPath "C:\cats" -BindingInformation $bindingInformation -CertificateThumbPrint $thumbPrint -CertStoreLocation $storeLocation -Protocol https
-$file = "C:\Windows\System32\drivers\etc\hosts"
-$hostfile = Get-Content $file
-$hostfile += "10.25.1.4   cats.internet.local"
-$hostfile += "10.25.1.5   cats.internet.local"
-Set-Content -Path $file -Value $hostfile -Force
